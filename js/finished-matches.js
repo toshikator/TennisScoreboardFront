319eed284b13977260ef797da79c7668ccb66914
toshik <diamondspb2015@gmail.com>
2026-06-13 23:13:54 +0300

    Add finished matches search functionality and improve related styles
    - Introduced `finished-matches.js` to handle search functionality for finished matches by player name.
    - Updated `finishedMatches.html` with new search form and empty state handling.
    - Enhanced CSS to support styles for new search forms, fields, and buttons.
    - Improved modularity by consolidating reusable form and input styles.

---
+++ js/finished-matches.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", () => {
+    initFinishedMatchesPage();
+});
+
+async function initFinishedMatchesPage() {
+    // const formFirstname = document.getElementById("matches-by-firstname-form");
+    // const formLastname = document.getElementById("matches-by-lastname-form");
+    const matchesSearchForm = document.getElementById("matches-search-form");
+
+
+    // formFirstname.addEventListener("submit", handleGetMatchesByFirstname);
+    // formLastname.addEventListener("submit", handleGetMatchesByLastname);
+    matchesSearchForm.addEventListener("submit", handleGetMatchesByFullname);
+
+    await renderMatches();
+}
+
+async function renderMatches() {
+    const matchesList = document.getElementById("matches-list");
+    const emptyMessage = document.getElementById("matches-empty-state");
+    const firstNameInput = document.getElementById("first-name-search");
+    const lastNameInput = document.getElementById("last-name-search");
+
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+
+    if (!matchesList) {
+        return;
+    }
+
+    try {
+        const matches = await Api.getMatchesByPlayer(firstName, lastName);
+
+        matchesList.innerHTML = "";
+
+        if (!matches || !matches.length) {
+            showElement(emptyMessage);
+            return;
+        }
+
+        hideElement(emptyMessage);
+
+        matches.forEach(match => {
+            const playerElement = createMatchElement(match);
+            matchesList.appendChild(playerElement);
+        });
+    } catch (error) {
+        console.error(error);
+        showMessage("Failed to load players");
+    }
+}
+
+async function handleCreatePlayer(event) {
+    event.preventDefault();
+
+    const firstNameInput = document.getElementById("first-name");
+    const lastNameInput = document.getElementById("last-name");
+
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+
+    if (!firstName && !lastName) {
+        showMessage("At least First name or last name is required");
+        return;
+    }
+
+    try {
+        await Api.createPlayer({
+            firstName: firstName,
+            lastName: lastName
+        });
+
+        firstNameInput.value = "";
+        lastNameInput.value = "";
+
+        await renderPlayers();
+    } catch (error) {
+        console.error(error);
+        showMessage(error.message);
+    }
+}
+
+function createMatchElement(match) {
+    const item = document.createElement("div");
+    item.className = "match-item";
+    item.textContent = getMatchInfo(match);
+
+    return item;
+}
+
+function getMatchInfo(match) {
+    const player1 = Api.getPlayer(match.player1Id);
+    const player2 = Api.getPlayer(match.player2Id);
+    const winner = match.winner === player1 ? player1 : player2;
+    return `${player1.firstName} ${player2.firstName}`;
+}
+
+function showElement(element) {
+    if (element) {
+        element.hidden = false;
+    }
+}
+
+function hideElement(element) {
+    if (element) {
+        element.hidden = true;
+    }
+}
+
+function showMessage(message) {
+    alert(message);
+}
